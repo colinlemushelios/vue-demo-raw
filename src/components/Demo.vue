d92e4942dcc1a4f404781a6abbdb6964e25e2616
Colin Lemus <colin.lemus@heliosinteractive.com>
2021-11-04 11:14:19 -0700

    Inital commit with two lines of text.

---
+++ src/components/Demo.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1>Hello there!</h1>
+    <h2>General Kenobi</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Demo",
+};
+</script>
+
+<style scoped>
+h1 {
+  font-size: 200%;
+  font-weight: bold;
+}
+</style>
